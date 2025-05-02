c7f3df604c7a78e9fed7169dca9e071d6a6c5b74
Seijo Cecilia <willamwei@outlook.com>
2025-05-02 21:00:32 +0800

    [protobuf] ready for maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.1.20"
     `java-library`
     `maven-publish`
+    id("org.jreleaser") version "1.18.0"
 }
 
 group = "org.ntqqrev.saltify"
@@ -25,6 +26,7 @@ kotlin {
 }
 
 java {
+    withJavadocJar()
     withSourcesJar()
 }
 
@@ -35,7 +37,57 @@ publishing {
             version = project.version.toString()
             artifactId = rootProject.name
 
+            pom {
+                name = "saltify-protobuf"
+                description = "ProtoBuf runtime solution for Kotlin JVM"
+                url = "https://github.com/SaltifyDev/saltify-protobuf"
+                inceptionYear = "2025"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Wesley-Young"
+                        name = "Wesley F. Young"
+                        email = "wesley.f.young@outlook.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/SaltifyDev/saltify-protobuf.git"
+                    developerConnection = "scm:git:ssh://github.com/SaltifyDev/saltify-protobuf.git"
+                    url = "https://github.com/SaltifyDev/saltify-protobuf"
+                }
+            }
+
             from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+jreleaser {
+    signing {
+        setActive("ALWAYS")
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                setActive("ALWAYS")
+                create("sonatype") {
+                    setActive("ALWAYS")
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                }
+            }
         }
     }
 }
